ee6d47169264edb795028d0cd324b33afee3537d
Zrexer <122536817+Zrexer@users.noreply.github.com>
2023-09-21 14:35:40 +0330

    Update md5Creator.py

--- md5Creator.py
+++ md5Creator.py
@@ -5,6 +5,6 @@ def generateMD5(text: str = None):
     md5.update(text.encode('utf-8'))
     return md5.hexdigest()
 
-myText = "Hello world"
+myText = str(input("text> "))
 
 print(generateMD5(myText))
